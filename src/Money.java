13e68fd1a50b823713840c49cf513f53d1ba6aee
v0o0v <v0o0v@daum.net>
2020-03-10 13:54:44 +0900

    gitignore 수정

--- src/Money.java
+++ src/Money.java
@@ -1,10 +1,20 @@
-public class Money {
+abstract public class Money {
     protected int amount;
+
+    abstract Money times(int i);
 
     @Override
     public boolean equals(Object o) {
         Money money = (Money) o;
         return amount == money.amount
                 && getClass().equals(money.getClass());
+    }
+
+    static Money dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    static Money franc(int amount) {
+        return new Franc(amount);
     }
 }
