34c6cb6781a4c96266084adeebf96508018e0981
v0o0v <v0o0v@daum.net>
2020-03-10 13:27:39 +0900

    gitignore 수정

--- test/TDDTest.java
+++ test/TDDTest.java
@@ -4,22 +4,23 @@ import static org.junit.Assert.*;
 
 public class TDDTest {
 
-    @org.junit.Test
+    @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
         assertEquals(new Dollar(10), five.times(2));
         assertEquals(new Dollar(15), five.times(3));
     }
 
-    @org.junit.Test
-    public void testEquality(){
+    @Test
+    public void testEquality() {
         assertTrue(new Dollar(5).equals(new Dollar(5)));
         assertFalse(new Dollar(5).equals(new Dollar(6)));
         assertTrue(new Franc(5).equals(new Franc(5)));
         assertFalse(new Franc(5).equals(new Franc(6)));
+        assertFalse(new Franc(5).equals(new Dollar(5)));
     }
 
-    @org.junit.Test
+    @Test
     public void testFrancMultiplication() {
         Franc five = new Franc(5);
         assertEquals(new Franc(10), five.times(2));
