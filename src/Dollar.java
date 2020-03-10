56869db9bd71e9fa45bb256749ba875ae9ddf669
v0o0v <v0o0v@daum.net>
2020-03-10 14:44:36 +0900

    gitignore 수정

--- src/Dollar.java
+++ src/Dollar.java
@@ -1,12 +1,15 @@
 public class Dollar extends Money {
 
-    public Dollar(int amount) {
-        this.amount = amount;
+    public Dollar(int amount, String currency) {
+        super(amount,currency);
     }
 
     Money times(int multiplier) {
-        return new Dollar(amount * multiplier);
+        return Money.dollar(amount * multiplier);
     }
 
-
+    @Override
+    public String currency() {
+        return "USD";
+    }
 }
